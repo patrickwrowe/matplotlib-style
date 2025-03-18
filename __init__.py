fb8a274bcb0b3e951b85e64d10d40a5e5e20790f
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-03-18 12:00:08 -0230

    Add init

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from .pr_theme import (
+    theme_categorical_bright, 
+    theme_categorical_pastel, 
+    theme_colours, 
+    setup_matplotlib_environment
+)
+
+__all__ = [
+    "theme_categorical_bright", 
+    "theme_categorical_pastel", 
+    "theme_colours", 
+    "setup_matplotlib_environment"
+]
